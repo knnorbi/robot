88651595ccb280bcb0d3e995a03a2f2f9b216f44
knnorbi <kluka.norbert@gmail.com>
2019-04-11 12:08:31 +0200

    Initial commit

---
+++ robot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UtasitasSorozat> utasitasSorozatok = new List<UtasitasSorozat>();
+
+            string[] sorok = File.ReadAllLines(@"c:\tmp\19-04-10_robotosdi\program.txt");
+
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                utasitasSorozatok.Add(new UtasitasSorozat(sorok[i]));
+            }
+
+            Console.WriteLine("2. feladat");
+            Console.WriteLine("Kérek egy szamot:");
+            int sorsz = -1;
+            try
+            {
+                sorsz = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Számot!!!!!!!");
+            }
+
+            try
+            {
+                Console.Write("A " + sorsz + ". utasítássorozat ");
+                Console.Write(utasitasSorozatok[sorsz].Egyszerusitheto ? "nem " : "");
+                Console.WriteLine("egyszerűsíthető!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Nincs ilyen utasítássorozat!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ robot/UtasitasSorozat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robot
+{
+    public class UtasitasSorozat
+    {
+        List<Irany> utasitasok;
+
+        public UtasitasSorozat()
+        {
+            utasitasok = new List<Irany>();
+        }
+
+        public UtasitasSorozat(string utasitasokString)
+        {
+            utasitasok = new List<Irany>();
+            for (int i = 0; i < utasitasokString.Length; i++)
+            {
+                switch (utasitasokString[i])
+                {
+                    case 'E': utasitasok.Add(Irany.Eszak); break;
+                    case 'D': utasitasok.Add(Irany.Del); break;
+                    case 'K': utasitasok.Add(Irany.Kelet); break;
+                    case 'N': utasitasok.Add(Irany.Nyugat); break;
+                }
+            }
+        }
+
+        public bool Egyszerusitheto
+        {
+            get
+            {
+                for (int i = 0; i < utasitasok.Count - 1; i++)
+                {
+                    if (utasitasok[i] == Irany.Del && utasitasok[i + 1] == Irany.Eszak)
+                        return true;
+                    if (utasitasok[i] == Irany.Kelet && utasitasok[i + 1] == Irany.Nyugat)
+                        return true;
+                    if (utasitasok[i] == Irany.Nyugat && utasitasok[i + 1] == Irany.Kelet)
+                        return true;
+                    if (utasitasok[i] == Irany.Eszak && utasitasok[i + 1] == Irany.Del)
+                        return true;
+                }
+
+                return false;
+            }
+        }
+
+    }
+}
